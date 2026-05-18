ee53298ce0eea0c98fb9ad95f7d2bfa70a63591a
Thomson0010 <i23koutto@oakm.cz>
2026-05-18 15:36:40 +0200

    18.5

---
+++ pujcovna-lodi/app.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.send("Aplikace je pripravena");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server bezi na adrese http://localhost:${PORT}`);
+});
